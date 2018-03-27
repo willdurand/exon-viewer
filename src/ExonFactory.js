9e436733fd14c2ea18dbaace2812e52fed5c668b
William Durand <will+git@drnd.me>
2018-03-27 20:14:07 +0200

    draw depth traces for each gene

--- src/ExonFactory.js
+++ src/ExonFactory.js
@@ -1,13 +1,19 @@
+// generate random colors
+const colors = Array(30).map(() => {
+  return '#'+'0123456789abcdef'.split('').map((v, i, a) => {
+    return i > 5 ? null : a[Math.floor(Math.random()*16)];
+  }).join('');
+});
+
 function exonFactory(exonPositions) {
   const exons_by_gene = {};
-
 
   for (let i = 0; i < exonPositions.length; i++) {
     let start = exonPositions[i].start;
     let end = exonPositions[i].end;
     let text = exonPositions[i].gene;
 
-    const v = exons_by_gene[text] || { x: [], y: [], text: [] };
+    const v = exons_by_gene[text] || { x: [], y: [], text: [], start, end };
 
     //v.x.push(Number(end) - Number(start));
     //v.x.push(0);
@@ -23,14 +29,24 @@ function exonFactory(exonPositions) {
     v.y.push(NaN);
     v.text.push(NaN);
 
+    if (start < v.start) {
+      v.start = start;
+    }
+
+    if (end > v.end) {
+      v.end = end;
+    }
+
     exons_by_gene[text] = v;
   }
 
-  return Object.keys(exons_by_gene).map((gene, index) => {
+  const axes = {};
+  const exons = Object.keys(exons_by_gene).map((gene, index) => {
     const { x, y, text } = exons_by_gene[gene];
 
-    const downscaledX = [];
+    const downscaledX = x;
 
+    /*
     let sum = 0;
     for (let i = 0; i < x.length; i += 3) {
       const len = x[i + 1] - x[i];
@@ -43,12 +59,17 @@ function exonFactory(exonPositions) {
 
       sum = sum + len + 10;
     }
+    */
 
-    return {
-      x: downscaledX,
-      y,
+    axes[gene] = {
       xaxis: index === 0 ? undefined : `x${index + 1}`,
       yaxis: index === 0 ? undefined : `y${index + 1}`,
+    };
+
+    return {
+      ...axes[gene],
+      x: downscaledX,
+      y,
       type: 'scattergl',
       text,
       connectgaps: false,
@@ -57,59 +78,96 @@ function exonFactory(exonPositions) {
       showlegend: false,
       line: {
         width: 8,
+        color: colors[index],
       }
     };
   });
+
+  return {
+    axes,
+    exons,
+    exons_by_gene,
+  };
 }
 
-function depthFactory(depths, names) {
+function depthFactory(depths, names, exons_by_gene, axes) {
 
   if (depths.length !== names.length) {
-      let e = new Error('# of depth measurements unequal to # of measurement names');
-      throw(e);
+    let e = new Error('# of depth measurements unequal to # of measurement names');
+    throw(e);
   }
 
-  let allTraces = [];
+  const allTraces = [];
+  const depths_by_name = {};
 
-  for(let i=0; i<depths.length; i++) {
+  for (let i = 0; i < depths.length; i++) {
+    const name = names[i];
+    const depths_by_gene = {};
 
-    let depth = depths[i];
-    let trace = {
-      mode: 'lines+markers',
-      type: 'scattergl',
-      connectgaps: false,
-      line: {width: 0.5},
-      marker: {size: 2},
-      name: names[i]
-    };
-    let x = [];
-    let y = [];
-    let text = [];
+    for (let j = 0; j < depths[i].length; j++) {
+      const depth = depths[i][j];
 
-    for(let j=0; j<depth.length; j++) {
-      // if(j % 1000 == 0) {
-        let d = depth[j];
+      Object.keys(exons_by_gene).forEach((gene) => {
+        const { start, end } = exons_by_gene[gene];
+        const v = depths_by_gene[gene] || new Set();
 
-        x.push(d.start);
-        x.push(d.end);
-        x.push(NaN);
+        if (depth.start >= start && depth.end <= end) {
+          v.add(depth);
+        }
 
-        y.push(d.depth);
-        y.push(d.depth);
-        y.push(NaN);
-
-        text.push(d.chromosome);
-        text.push(d.chromosome);
-        text.push(NaN);
-      // }
+        depths_by_gene[gene] = v;
+      });
     }
 
-    trace.x = x;
-    trace.y = y;
-    trace.text = text;
-
-    allTraces.push(trace);
+    depths_by_name[name] = depths_by_gene;
   }
+
+  Object.keys(depths_by_name).forEach((name, index) => {
+    const depths_by_gene = depths_by_name[name];
+
+    let showlegend = true;
+    Object.keys(depths_by_gene).forEach((gene) => {
+      const trace = {
+        ...axes[gene],
+        mode: 'markers',
+        type: 'scattergl',
+        connectgaps: false,
+        line: {
+          width: 1,
+          color: colors[index],
+        },
+        marker: {
+          size: 2,
+          color: colors[index],
+        },
+        name,
+        legendgroup: name,
+        showlegend,
+        x: [],
+        y: [],
+        text: [],
+      };
+
+      // show legend only for the first trace for each depth name
+      showlegend = false;
+
+      depths_by_gene[gene].forEach((depth) => {
+        trace.x.push(depth.start);
+        trace.x.push(depth.end);
+        trace.x.push(NaN);
+
+        trace.y.push(depth.depth);
+        trace.y.push(depth.depth);
+        trace.y.push(NaN);
+
+        trace.text.push(depth.chromosome);
+        trace.text.push(depth.chromosome);
+        trace.text.push(NaN);
+      });
+
+      allTraces.push(trace);
+    });
+  });
 
   return allTraces;
 }
